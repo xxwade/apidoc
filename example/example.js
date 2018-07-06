974f3a049b005c974edf57bfd9961d63381a0e0b
许鑫 <xuxin@xinhehui.com>
2018-07-06 13:46:54 +0800

    添加speed

--- example/example.js
+++ example/example.js
@@ -4,7 +4,7 @@
  * @apiName GetUser
  * @apiGroup User
  * @apiPermission admin
- *
+ * @apiSpeed 100ms
  * @apiDescription Compare Verison 0.3.0 with 0.2.0 and you will see the green markers with new items in version 0.3.0 and red markers with removed items since 0.2.0.
  *
  * @apiParam {Number} id The Users-ID.
